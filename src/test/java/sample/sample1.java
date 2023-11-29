1cfcfec9e255cb12468ac794fe7319035bd73c90
Rakesh111296 <raki111296@gmail.com>
2023-11-29 16:09:01 +0530

    comi23

---
+++ src/test/java/sample/sample1.java
@@ -0,0 +1,22 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class sample1 {
+	
+	@Test
+	public void test11 () {
+		System.out.println("Hi Test11");
+	}
+	
+	@Test
+	public void test12 () {
+		System.out.println("Hi test12");
+	}
+	
+	@Test
+	public void test13 () {
+		System.out.println("Hi test13");
+	}
+
+}
