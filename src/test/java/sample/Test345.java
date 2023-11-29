1f4f671f1de63982fef1480726f282efbcbd334b
Rakesh111296 <raki111296@gmail.com>
2023-11-29 15:59:17 +0530

    comit

--- src/test/java/sample/Test345.java
+++ src/test/java/sample/Test345.java
@@ -5,17 +5,17 @@ import org.testng.annotations.Test;
 public class Test345 {
 
 	@Test
-	public void demo () {
-		System.out.println("Hi Demo");
+	public void test () {
+		System.out.println("Hi Test");
 	}
 	
 	@Test
-	public void demo1 () {
-		System.out.println("Hi Demo1");
+	public void test1 () {
+		System.out.println("Hi test1");
 	}
 	
 	@Test
-	public void demo2 () {
-		System.out.println("Hi Demo2");
+	public void test2 () {
+		System.out.println("Hi test2");
 	}
 }
---
+++ src/test/java/sample/sample.java
@@ -0,0 +1,21 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class sample {
+
+	@Test
+	public void sample3 () {
+		System.out.println("Hi sample");
+	}
+	
+	@Test
+	public void sample1 () {
+		System.out.println("Hi sample1");
+	}
+	
+	@Test
+	public void sample2 () {
+		System.out.println("Hi sample2");
+	}
+}
