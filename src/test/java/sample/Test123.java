f337a2dd5fea87e6263c45ce533cd8792a23a2df
Rakesh111296 <raki111296@gmail.com>
2023-11-29 15:33:54 +0530

    commit

---
+++ src/test/java/sample/Test123.java
@@ -0,0 +1,22 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Test123 {
+
+	
+	@Test
+	public void demo () {
+		System.out.println("Hi Demo");
+	}
+	
+	@Test
+	public void demo1 () {
+		System.out.println("Hi Demo1");
+	}
+	
+	@Test
+	public void demo2 () {
+		System.out.println("Hi Demo2");
+	}
+}
---
+++ src/test/java/sample/Test345.java
@@ -0,0 +1,21 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Test345 {
+
+	@Test
+	public void demo () {
+		System.out.println("Hi Demo");
+	}
+	
+	@Test
+	public void demo1 () {
+		System.out.println("Hi Demo1");
+	}
+	
+	@Test
+	public void demo2 () {
+		System.out.println("Hi Demo2");
+	}
+}
